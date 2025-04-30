bce028cbe1985ed39099283a1341872f03ad275a
ParryingDagger <503679939@qq.com>
2025-04-30 19:25:39 +0800

    init

---
+++ src/main/java/com/jrj/transaction/TransactionApplication.java
@@ -0,0 +1,25 @@
+package com.jrj.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class TransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins("http://127.0.0.1:9876");
+			}
+		};
+	}
+}
